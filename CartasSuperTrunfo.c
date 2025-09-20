81c21a39fa6e0f662c3211d66eb31f0e650aebb7
AdelsonDev <adelsonmarques10@gmail.com>
2025-09-20 21:03:44 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,23 +2,51 @@
 
 
 int main() {
-  // carta 1
-  char estado1;
-  char codigo1[4];
-  char nomeCidade1[40];
-  int populacao1;
-  float area1;
-  float pib1;
-  int numeroPontosTuristicos1;
+  // Variáveis carta 1
+  char estado1;                       // Estado de A até H
+  char codigo1[4];                    // código da carta
+  char nomeCidade1[40];               // Nome da cidade até 40 caractere
+  int populacao1;                     // População da cidade, número inteiro
+  float area1;                        // Área em km²
+  float pib1;                         // produto interno bruto da cidade, número decimal
+  int numeroPontosTuristicos1;        // Número de pontos turísticos, número interiro
 
   // carta 2
-  char estado2;
-  char codigo2[4];
-  char nomeCidade2[40];
-  int populacao2;
-  float area2;
-  float pib2;
-  int numeroPontosTuristicos2;
+  char estado2;                       // Estado de A até H
+  char codigo2[4];                    // Código da carta
+  char nomeCidade2[40];               // Nome da cidade até 40 caractere
+  int populacao2;                     // População da cidade, número inteiro    
+  float area2;                        // Área em km²
+  float pib2;                         // Produto interno bruto da cidade, número decimal   
+  int numeroPontosTuristicos2;        // Número de pontos turísticos, número interiro
+
+
+
+// Entrada de dados da carta 1
+
+
+printf("Digite o estado da carta 1 (A-H): \n");
+scanf( "%c", &estado1); // lê 1 caractere
+
+printf("Digite o código da carta 1: \n");
+scanf("%c", &codigo1); // lê strins sem espaços
+
+printf("Digite o nome da cidade 1: \n");
+scanf("%c", &nomeCidade1);
+
+printf("Digite a população da cidade 1: \n");
+scanf("%d", &populacao1); // lê número inteiro
+
+
+printf("Digite a area da cidade1(km²): \n");
+scanf("%f", &area1);      // lê número decimal
+
+printf("Digite o PIB da cidade 1: \n");
+scanf("%f", &pib1);       // lê número decimal
+
+printf("Digite o numero de pontos turisticos 1: \n");
+scanf("%d", &numeroPontosTuristicos1); // lê número inteiro
+
 
 
 
